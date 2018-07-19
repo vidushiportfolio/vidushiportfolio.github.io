fc85b33a68d0d88d54dd53788580072268ac5015
vidushi-agarwal <vidushiagarwal1997@gmail.com>
2018-07-19 15:27:01 +0530

    .

--- js_2/paroller_index.js
+++
@@ -1,7 +0,0 @@
-
-$(function(){
-    // $("h1").paroller({ factor: '0.5', type: 'foreground'});
-    $("#just1").paroller({ factor: '-1', type: 'foreground', direction: 'horizontal' });
-	$("#just2").paroller({ factor: '1', type: 'foreground', direction: 'horizontal' });
-	// $("footer.secscreen").paroller({ factor: '-0.3', type: 'foreground', direction: 'horizontal' });
-	});
